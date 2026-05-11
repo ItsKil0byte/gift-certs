6a8472a283ea6f68167df64cb7bbb76d769bb25f
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-11 15:56:24 +0500

    (feat): добавлена модель и миграции для таблицы дизайнов

---
+++ app/Models/Design.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['title, image_path'])]
+class Design extends Model
+{
+    // TODO
+}
