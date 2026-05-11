0fcffd7b4f61f2b3bf066c27cc96eec26c6861bb
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-11 22:28:57 +0500

    (feat): добавил сидер для дизайнов сертификатов

---
+++ database/seeders/DesignSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Design;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DesignSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Design::insert([
+            [
+                'title' => 'Design 1',
+                'image_path' => 'designs/1.png',
+                'is_active' => true,
+                'created_at' => now()->subDays(1),
+                'updated_at' => now()->subDays(1)
+            ],
+            [
+                'title' => 'Design 2',
+                'image_path' => 'designs/2.png',
+                'is_active' => true,
+                'created_at' => now()->subDays(1),
+                'updated_at' => now()->subDays(1)
+            ],
+            [
+                'title' => 'Design 3',
+                'image_path' => 'designs/3.png',
+                'is_active' => true,
+                'created_at' => now()->subDays(1),
+                'updated_at' => now()->subDays(1)
+            ]
+        ]);
+    }
+}
