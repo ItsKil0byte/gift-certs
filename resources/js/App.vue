bfc13accbddab0e5f48ea8ad087a29d2ac71abe3
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-17 17:40:45 +0500

    (feat): добавил базовую загрузку объект с бэка в фронт

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,39 @@
+<script setup>
+import axios from 'axios';
+import { ref, onMounted, computed } from 'vue';
+
+// TODO: Убрать в отдельный файл
+
+const api = axios.create({
+    baseURL: '/api',
+});
+
+const designs = ref([]);
+const nominals = ref([]);
+
+const selectedDesign = ref(null);
+const selectedNominal = ref(null);
+
+const currentDesign = computed(() => {
+    return designs.value.find(design => design.id === selectedDesign.value);
+});
+
+onMounted(async () => {
+    try {
+        const designs_response = await api.get('/designs');
+        designs.value = designs_response.data.data;
+
+        const nominals_response = await api.get('/nominals');
+        nominals.value = nominals_response.data.data;
+
+        console.log(designs.value);
+        console.log(nominals.value);
+    } catch (error) {
+        console.error(error);
+    }
+})
+</script>
+
 <template>
 
     <!-- TODO: Разбить на компоненты -->
@@ -40,13 +76,19 @@
                         <h2 class="text-xl font-bold mb-4">Предпросмотр</h2>
                         <div
                             class="w-full aspect-3/2 bg-slate-100 rounded-lg border-2 border-dashed border-slate-300 flex items-center justify-center">
-                            Сертификат
+                            <span v-if="currentDesign" class="text-slate-700 text-2xl text-bold">
+                                {{ currentDesign.title }}
+                            </span>
+                            <span v-else class="text-slate-400">Выберите дизайн</span>
                         </div>
 
                         <h2 class="text-xl font-bold mb-4 mt-4">Выберите дизайн</h2>
-                        <div
-                            class="h-32 bg-slate-100 rounded-lg border-2 border-dashed border-slate-300 flex items-center justify-center">
-                            Слайдер дизайнов
+                        <div class="grid grid-cols-4 gap-4">
+                            <div v-for="design in designs" :key="design.id" @click="selectedDesign = design.id"
+                                :class="selectedDesign === design.id ? 'border-indigo-500 bg-indigo-100' : 'border-dashed border-slate-300 bg-slate-100'"
+                                class="w-full aspect-3/2 rounded-lg border-2 flex items-center justify-center cursor-pointer transition-colors">
+                                {{ design.title }}
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -54,9 +96,12 @@
                 <div class="lg:col-span-5 bg-white p-6 rounded-xl border-2 border-slate-200 flex flex-col gap-6">
                     <h2 class="text-xl font-bold">Настроить сертификат</h2>
 
-                    <div
-                        class="h-32 bg-slate-100 rounded-lg border-2 border-dashed border-slate-300 flex items-center justify-center">
-                        1. Номинал
+                    <div class="grid grid-cols-4 gap-4">
+                        <div v-for="nominal in nominals" :key="nominal.id" @click="selectedNominal = nominal.id"
+                            :class="selectedNominal === nominal.id ? 'border-indigo-500 bg-indigo-100' : 'border-dashed border-slate-300 bg-slate-100'"
+                            class="w-full h-16 rounded-lg border-2 flex items-center justify-center cursor-pointer transition-colors">
+                            {{ nominal.value }}
+                        </div>
                     </div>
 
                     <div
