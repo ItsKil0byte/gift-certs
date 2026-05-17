f94c91cf97727a2082c743f289bd811260381884
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-18 00:29:50 +0500

    (feat): добавлен ресурс для сертификата

---
+++ app/Http/Resources/CertificateResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CertificateResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'promo_code' => $this->promo_code,
+            'hash' => $this->hash
+        ];
+    }
+}
