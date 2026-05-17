9c468b0b30fafe10512638d81032e62545c8b9a0
Alexander Lebedev <lebalexevg@gmail.com>
2026-05-18 02:15:22 +0500

    (refactor): подключено и настроено форматирование для файлов проекта

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import { bunny } from 'laravel-vite-plugin/fonts'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
@@ -25,7 +25,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            vue: "vue/dist/vue.esm-bundler.js"
-        }
-    }
-});
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+})
